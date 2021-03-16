e3fa271a1ba1831a849e08cc4422a21a13e56a38
Melina <47388675+Klochette@users.noreply.github.com>
2021-03-16 14:17:47 +0100

    add all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+function Modal({ open, onClose }) {
+  return (
+    <div className="home__modal" style={{ display: open ? "flex" : "none" }}>
+      <svg
+        className="modal__heart"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 36 36"
+      >
+        <path
+          fill="#ff5075"
+          d="M35.885 11.833c0-5.45-4.418-9.868-9.867-9.868-3.308 0-6.227 1.633-8.018 4.129-1.791-2.496-4.71-4.129-8.017-4.129-5.45 0-9.868 4.417-9.868 9.868 0 .772.098 1.52.266 2.241C1.751 22.587 11.216 31.568 18 34.034c6.783-2.466 16.249-11.447 17.617-19.959.17-.721.268-1.469.268-2.242z"
+        />
+      </svg>
+      <p className="modal__title">Bisouuuuus !</p>
+      <button className="modal__button" onClick={() => onClose(false)}>
+        retour
+      </button>
+    </div>
+  );
+}
 
 function App() {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <h1>Coucou ZoK !</h1>
+      <button onClick={() => setOpen(true)} className="home__button">
+        click me !
+      </button>
+      <Modal open={open} onClose={(close) => setOpen(close)} />
     </div>
   );
 }
